e1db8ff9c90127ad6cd0bf41d29f591067523310
supriyapuri <supriyapuri11@gmail.com>
2020-07-14 15:52:05 -0700

    changes made for calendar and all test pass

--- daos/calendars.js
+++ daos/calendars.js
@@ -6,6 +6,10 @@ module.exports.create = async (name) => {
   return await Calendars.create({ name });
 };
 
+module.exports.updateById= async(calendarId, calendar) => {
+  return await Calendars.update({_id: calendarId}, calendar)
+};
+
 module.exports.getById = async (id) => {
   try {
     const calendar = await Calendars.findOne({ _id: id }).lean();
@@ -14,3 +18,28 @@ module.exports.getById = async (id) => {
     return null;
   }
 };
+
+module.exports.deleteById = async(id) => {
+  await Calendars.remove({_id: id});
+}
+
+module.exports.getAllCalendars = async()=> {
+  try {
+    const calendars = await Calendars.find({});
+    console.log(calendars);
+    return calendars
+  } catch (e) {
+    return null;
+  }
+}
+
+
+// const calendars = await Calendars.find(id);
+//     calendars.array.forEach(element => {
+//       return element
+
+
+
+
+// db.c.find({}, {_id:1}).map(function(item){ return item._id; })
+
--- routes/calendars.js
+++ routes/calendars.js
@@ -3,23 +3,83 @@ const router = Router();
 
 const CalendarDAO = require('../daos/calendars');
 
+
+// create
 router.post("/", async (req, res, next) => {
   const { name } = req.body;
   if (!name) {
     res.status(400).send('body parameter "name" is required"');
-  } else {
-    const calendar = await CalendarDAO.create(name);
-    res.json(calendar);
+    } 
+  else {
+    try{
+      const calendar = await CalendarDAO.create(name);
+      res.json(calendar);
+      }
+    catch(e) {
+      res.status(500).send(e.message);
+    }
+    
   }
 });
 
+//read - single calendar id
+
 router.get("/:id", async (req, res, next) => {
+  // const calendarId= req.params.id;
   const calendar = await CalendarDAO.getById(req.params.id);
   if (calendar) {
     res.json(calendar);
-  } else {
+    } 
+    else {
     res.sendStatus(404);
-  }
+    }
 });
+
+
+// update
+
+router.put("/:id", async (req, res, next) => {
+  const calendarId= req.params.id;
+  const calendar = req.body;
+  
+  if (!calendar || JSON.stringify(calendar)=== '{}'){
+    res.status(400).send('calendar is required');
+  } else
+  {
+    const updatedCalendar = await CalendarDAO.updateById(calendarId, calendar);
+    res.json(updatedCalendar)
+  } 
+});
+
+// delete
+router.delete("/:id", async (req, res, next) => {
+  const calendarId = req.params.id;
+  try {
+    await CalendarDAO.deleteById(calendarId);
+    res.sendStatus(200);
+    } catch(e){
+      res.status(500).send(e.message);
+    }
+    
+});
+
+//get all
+
+router.get("/", async (req, res, next) => {
+  
+  const calendar = await CalendarDAO.getAllCalendars();
+  console.log(calendar);
+  if (calendar) {
+    res.json(calendar);
+    } 
+    else {
+    res.sendStatus(404);
+    }
+});
+
+
+
+
+
 
 module.exports = router;
