e8f76b96f41dd93285ab704f97dda71f664cde9b
supriyapuri <supriyapuri11@gmail.com>
2020-07-19 14:03:01 -0700

    updated the calendar model

--- models/calendars.js
+++ models/calendars.js
@@ -1,13 +1,7 @@
 const mongoose = require('mongoose');
 
-// const eventSchema = new mongoose.Schema({
-//   name: { type: String, required: true },
-//   data: {type: Date, required: true}
-// });
-
 const calendarSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  // events: {type: [eventSchema]}
 
 });
 
